6d9b552f059f678c92df58c779ab2484444f6043
unknown <vamsi3353@gmail.com>
2020-07-03 14:43:44 +0530

    FD1575285

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,17 +1,79 @@
 // Progression #1: Greatest of the two numbers
+function greatestOfTwoNumbers() {
+  let a = prompt("enter the first number");
+  let b = prompt("enter the second number");
+  if (a > b) {
+    return a;
+  } else if (b > a) {
+    return b;
+  } else {
+    return null;
+  }
+}
+
 
 // Progression #2: The lengthy word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+var lgth = 0;
+var longest;
+
+for (var i = 0; i < words.length; i++) {
+  if (arr[i].length > lgth) {
+    var lgth = words[i].length;
+    longest = words[i];
+  }
+}
+
+alert(longest);
 
 // Progression #3: Net Price
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
+var sum = 0;
+
+for (var i = 0; i < numbers.length; i++) {
+
+  sum += numbers[i]
+
+}
+
+console.log(sum);
+
 // Progression #4: Calculate the average
 // Progression 4.1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+var sum = 0;
+var avg = 0;
+
+for (var i = 0; i < numbersAvg.length; i++) {
+
+  sum += numbers[i]
+
+}
+avg = sum / numbersAvg.length;
+console.log(avg);
+
+
+
 
 // Progression 4.2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+
+function averageWordLength(wordsArr) {
+  var avg = 0;
+  var len = 0;
+  for (var i = 0; i < wordsArr.length; i++) {
+    len += wordsArr[i].length;
+  }
+  avg = len / wordsArr.length;
+  if (wordsArr.length === 0) {
+    return null;
+  } else {
+    return avg;
+  }
+
+}
+
 
 // Progression #5: Unique arrays
 const wordsUnique = [
@@ -29,8 +91,29 @@ const wordsUnique = [
   'flour'
 ];
 
+
+
 // Progression #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+
+function findElement(wordsFind, wordtoSearch) {
+  var lenth = wordsFind.length;
+  var isAvailable = false;
+  for (var i = 0; i < lenth; i++) {
+    if (wordsFind[i] === wordtoSearch) {
+      isAvailable = true;
+    }
+  }
+  if (lenth === 0) {
+    return null;
+  } else if (isAvailable === true) {
+    return true
+  } else if (isAvailable === false) {
+    return wordsFind;
+  }
+
+}
+
 
 // Progression #7: Count repetition
 const wordsCount = [
@@ -46,6 +129,7 @@ const wordsCount = [
   'disobedience',
   'matter'
 ];
+
 
 // Progression #8: Bonus
 
